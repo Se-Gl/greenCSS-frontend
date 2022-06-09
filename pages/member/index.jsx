d7e8059d9256c7cc88050ecfb9efa8aa9e62bc07
Se-Gl <me@severinglaser.ch>
2022-06-09 02:42:49 +0200

    updated member hero section

--- pages/member/index.jsx
+++ pages/member/index.jsx
@@ -1,4 +1,5 @@
 import dynamic from 'next/dynamic'
+import Image from 'next/image'
 import SEO from '@/components/reusable/SEO'
 import MemberTierCard from '@/components/member/MemberTierCard'
 
@@ -12,15 +13,27 @@ export default function index({ stripeData }) {
       url='member'
       keywords='member, donation, green software, sustainable software'>
       <Layout className='container sm:px-10px md:px-25px lg:px-50px min-h-100vh'>
-        <div className='flex h-100vh'>
-          <div className='m-auto text-center'>
-            <h1>Choose your plan</h1>
-            <p className='text-black-10'>
-              You decide where you donate. Compare plans, then choose the one just right for you.
-            </p>
+        <div className='relative h-100vh bg-blue-8'>
+          <Image
+            layout='fill'
+            objectFit='contain'
+            className='object-cover pointer-events-none'
+            src='/images/member/member-hero.webp'
+            alt='greenCSS Memberhero'
+            placeholder='blur'
+            blurDataURL='/_next/image?url=/images/member/member-hero.webp&w=16&q=1'
+          />
+          <div className='text-center absolute right-0per left-0per top-0per bottom-0per m-auto'>
+            <div className='relative top-50per translate-y-neg-50per'>
+              <h1 className='sm:rel-text-7vw md:rel-text-7vw lg:rel-text-7vw text-80px'>Choose your plan</h1>
+              <p className='max-w-60rem sm:max-w-30rem md:max-w-30rem mx-auto'>
+                You decide where you donate. Compare plans, then choose the one just right for you.
+              </p>
+            </div>
           </div>
         </div>
-        <>
+
+        <div className='my-100px'>
           <div className='mx-auto text-center'>
             <h2>Compare your benefits</h2>
             <p className='text-black-10'>Not sure which plan to choose? Let us find the one just right for you</p>
@@ -32,7 +45,7 @@ export default function index({ stripeData }) {
               <MemberTierCard key={i} title={d.nickname} price={d.unit_amount / 100} />
             ))}
           </div>
-        </>
+        </div>
       </Layout>
     </SEO>
   )
