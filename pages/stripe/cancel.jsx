f2529010a8d8978d25beee8b3f03c0c6913d7760
Se-Gl <me@severinglaser.ch>
2022-06-18 16:55:02 +0200

    adjusted stripe/cancel page to match new design

--- pages/stripe/cancel.jsx
+++ pages/stripe/cancel.jsx
@@ -1,6 +1,7 @@
 import dynamic from 'next/dynamic'
 import SEO from '@/components/reusable/SEO'
-import SubSectionHero from '@/components/grid/SubSectionHero'
+import ModernGrid from '@/components/grid/ModernGrid'
+import { GreenButton } from '@/components/reusable/Button'
 
 const Layout = dynamic(() => import('@/components/reusable/Layout'), { ssr: false })
 
@@ -12,7 +13,18 @@ export default function stripeCancel() {
       description='greenCSS membership Payment - error.'
       url='/stripe/cancel'>
       <Layout className='container min-h-100vh relative z-2'>
-        <SubSectionHero header='Oops!' subheader='Something has gone wrong. Please try again later or log in again.' />
+        <ModernGrid
+          header='Oops! - Please forgive me!'
+          subheader='Something has gone wrong. Please try again later or log in again.'
+          imageBg='blue'
+          imageUrl='/images/404/404-greencss.webp'
+          imageAlt='greencss 404 not found'
+        />
+        <div className='w-100per mb-50px'>
+          <GreenButton href='/member/account' hasLink={true} isDefault={false}>
+            Member area
+          </GreenButton>
+        </div>
       </Layout>
     </SEO>
   )
