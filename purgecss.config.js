b8d91591c7553820f187042b28a7f679ca689ebf
Se-Gl <me@severinglaser.ch>
2022-06-21 20:27:05 +0200

    updated purgecss config

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,7 +1,27 @@
 module.exports = {
-  content: ['pages/**/*.{js,jsx,ts,tsx}', 'components/**/*.{js,jsx,ts,tsx}', 'utils/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    'pages/**/*.{js,jsx,ts,tsx}',
+    'components/**/*.{js,jsx,ts,tsx}',
+    'data/**/*.{js,jsx,ts,tsx}',
+    'utils/**/*.{js,jsx,ts,tsx}'
+  ],
   css: ['./styles/greenCSS.css'],
   defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-  safelist: ['html', 'body'],
+  safelist: {
+    greedy: [
+      /red$/,
+      /orange$/,
+      /yellow$/,
+      /green$/,
+      /greencss$/,
+      /blue$/,
+      /turquoise$/,
+      /purple$/,
+      /magenta$/,
+      /black$/,
+      /transparent$/,
+      /white$/
+    ]
+  },
   output: './css/green.css'
 }
