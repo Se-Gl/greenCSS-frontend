739d6f0ad02c8f70a7fbb444fcef30bb096f8ec0
Se-Gl <me@severinglaser.ch>
2022-07-15 01:24:40 +0200

    fix(purgecss): fixed purgecss path to greencss module

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   content: ['pages/**/*.{js,jsx,ts,tsx}', 'components/**/*.{js,jsx,ts,tsx}', 'utils/**/*.{js,jsx,ts,tsx}'],
-  css: ['./styles/greenCSS.css'],
+  css: ['./node_modules/greencss/css/minified/greencss.css'],
+  keyframes: true,
   defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
   safelist: ['html', 'body'],
   output: './css/green.css'
