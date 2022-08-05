aadf615e356be0582c3cbb568dabbcb9e69bcc35
Se-Gl <me@severinglaser.ch>
2022-08-05 18:13:13 +0200

    perf(hero): deleted unused snippets

--- components/LandingPage/Hero.jsx
+++ components/LandingPage/Hero.jsx
@@ -96,32 +96,6 @@ export default function Hero() {
           </div>
         </div>
       </div>
-
-      {/* <ModernGrid
-        id='hero-section'
-        isHero={true}
-        isFull={true}
-        imagePosition={true}
-        titleStyle='text-white'
-        descriptionStyle='text-white'
-        header='A classy way to write sustainable CSS'
-        subheader='Styling CSS reimagined - for everyone. Start to code with zero emission: for your clients, for your
-  creativity and for your planet.'
-        imageBg='black'
-        imageUrl='/images/landingpage/hero-image.png'
-        imageAlt='wolf 3d hero greencss'>
-        <div className='flex sm:block gap-10px'>
-          <ReusableModal isHero={true} />
-          <GreenButton
-            hasLink={true}
-            isDefault={false}
-            href='#calculate-footprint'
-            id='linkedbutton'
-            className='text-white min-w-14rem font-500 sm:mt-10px'>
-            Calculate Footprint
-          </GreenButton>
-        </div>
-      </ModernGrid> */}
     </section>
   )
 }
