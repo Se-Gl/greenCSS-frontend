8f4b36d23b5aa68f3d75181b954539b49b22d2b8
Se-Gl <me@severinglaser.ch>
2022-08-06 11:45:31 +0200

    refactor(member): re-designed member sponsor card

--- components/LandingPage/Sponsor/SponsorCard.jsx
+++ components/LandingPage/Sponsor/SponsorCard.jsx
@@ -25,7 +25,12 @@ export default function SponsorCard({
   return (
     <div onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} key={index} onKeyDown={onKeyDown}>
       <div
-        className={`relative shadow-small-gray rounded-20px cursor-pointer min-h-35rem ${bgColor}`}
+        className={`relative shadow-small-gray rounded-20px cursor-pointer min-h-35rem ${
+          (index === 0 && 'bg-white') ||
+          (index === 1 && 'bg-blue') ||
+          (index === 2 && 'bg-black') ||
+          (bgColor && bgColor)
+        }`}
         onClick={onClick}>
         {imageUrl && (
           <div className='relative min-h-25rem sm:h-25per md:h-25per w-100per'>
@@ -34,9 +39,9 @@ export default function SponsorCard({
               layout='fill'
               objectFit='cover'
               src={
-                (index === 0 && '/images/landingpage/donation/planet-01.webp') ||
-                (index === 1 && '/images/landingpage/donation/planet-02.webp') ||
-                (index === 2 && '/images/landingpage/donation/planet-03.webp') ||
+                (index === 0 && '/images/landingpage/donation/planet-01.png') ||
+                (index === 1 && '/images/landingpage/donation/planet-02.png') ||
+                (index === 2 && '/images/landingpage/donation/planet-03.png') ||
                 (imageUrl && imageUrl)
               }
               alt={
@@ -52,15 +57,44 @@ export default function SponsorCard({
         )}
 
         <div className='px-25px py-50px'>
-          <h3 className={`mb-10px ${headerClass}`}>{cart ? cart.header : header}</h3>
-          <h4 className={`text-20px font-500 ${priceClass}`}>{cart ? cart.price : price}</h4>
-          <p className={`text-15px mb-0px ${descriptionClass}`}>{cart ? cart.subheader : subheader}</p>
+          <h3
+            className={`mb-10px ${
+              (index === 0 && 'text-black') ||
+              (index === 1 && 'text-white') ||
+              (index === 2 && 'text-white') ||
+              (headerClass && headerClass)
+            }`}>
+            {cart ? cart.header : header}
+          </h3>
+          <h4
+            className={`text-20px font-500 ${
+              (index === 0 && 'text-greencss') ||
+              (index === 1 && 'text-white') ||
+              (index === 2 && 'text-gray') ||
+              (priceClass && priceClass)
+            }`}>
+            {cart ? cart.price : price}
+          </h4>
+          <p
+            className={`text-15px mb-0px ${
+              (index === 0 && 'text-gray') ||
+              (index === 1 && 'text-white') ||
+              (index === 2 && 'text-white') ||
+              (descriptionClass && descriptionClass)
+            }`}>
+            {cart ? cart.subheader : subheader}
+          </p>
           <CtaButton
             index={index}
             hover={hover}
             className='absolute bottom-2per right-2per'
-            fillOne={fillOne}
-            fillTwo={fillTwo}
+            fillOne={fillOne && fillOne}
+            fillTwo={
+              (index === 0 && 'fill-black') ||
+              (index === 1 && 'fill-white') ||
+              (index === 2 && 'fill-gray') ||
+              (fillTwo && fillTwo)
+            }
           />
         </div>
       </div>
