f760244d881eee3148ddfa17b76a3fb3626f4b16
Se-Gl <me@severinglaser.ch>
2022-08-06 13:56:24 +0200

    fix(css): fixed example animations not showing in production

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,8 +1,12 @@
 module.exports = {
-  content: ['pages/**/*.{js,jsx,ts,tsx}', 'components/**/*.{js,jsx,ts,tsx}', 'utils/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    'pages/**/*.{js,jsx,ts,tsx}',
+    'components/**/*.{js,jsx,ts,tsx}',
+    'utils/**/*.{js,jsx,ts,tsx}',
+    'data/**/*.{js,jsx,ts,tsx}'
+  ],
   css: ['./node_modules/greencss/css/minified/greencss.css'],
   keyframes: true,
   defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-  safelist: ['html', 'body'],
   output: './css/green.css'
 }
